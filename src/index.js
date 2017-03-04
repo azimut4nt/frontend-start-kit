8f5d1bf4dbd77cb1f18ca27a17d2914e8d6833b0
RALZINOV\azimu <azimut.nt@gmail.com>
2017-03-04 21:10:10 +0300

    Added babel loader. Preset es2015

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import random from 'lodash/random';
+
+const t = () => {
+	console.log('Ping!', random(99))
+};
+
+t();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+const path = require('path');
+
+const pathTo = (pathString) => {
+  return path.resolve(__dirname, pathString);
+}
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: './build',
-		filename: 'bundle.js',
-	}
+  entry: pathTo('./src/index.js'),
+  output: {
+    path: pathTo('./build/'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: pathTo('./node_modules/'),
+        options: {
+            plugins: ['transform-runtime'],
+            presets: ['es2015'],
+        }
+      }
+    ]
+  }
 };
